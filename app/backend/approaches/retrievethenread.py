a75ef2fe8c05bf4db5dc37bab3e3f45612aee923
Jon Rippon <jon@rippon.me>
2024-03-25 12:40:46 +0000

    Udate to ask a question retrival

--- app/backend/approaches/retrievethenread.py
+++ app/backend/approaches/retrievethenread.py
@@ -22,7 +22,7 @@ class RetrieveThenReadApproach(Approach):
     """
 
     system_chat_template = (
-        "You are an intelligent assistant helping Contoso Inc employees with their healthcare plan questions and employee handbook questions. "
+        "You are a DORA the  Digital Operational Resilience Act, DORA falls into 5 pillars."
         + "Use 'you' to refer to the individual asking the questions even if they ask with 'I'. "
         + "Answer the following question using only the data provided in the sources below. "
         + "For tabular information return it as an html table. Do not return markdown format. "
@@ -32,13 +32,12 @@ class RetrieveThenReadApproach(Approach):
 
     # shots/sample conversation
     question = """
-'What is the deductible for the employee plan for a visit to Overlake in Bellevue?'
+'what are the articles that need to be addressed by Small Enterprises within chapter 2 of the DORA act?'
 
 Sources:
-info1.txt: deductibles depend on whether you are in-network or out-of-network. In-network deductibles are $500 for employee and $1000 for family. Out-of-network deductibles are $1000 for employee and $2000 for family.
-info2.pdf: Overlake is in-network for the employee plan.
-info3.pdf: Overlake is the name of the area that includes a park and ride near Bellevue.
-info4.pdf: In-network institutions include Overlake, Swedish and others in the region
+The Dora Act.pdf#page=38: Articles 5 to 15 of this Regulation shall not apply to small and non-interconnected investment firms, payment
+institutions exempted pursuant to Directive (EU) 2015/2366.
+The Dora Act.pdf#page=38: identify key dependencies on ICT third-party service providers;.
 """
     answer = "In-network deductibles are $500 for employee and $1000 for family [info1.txt] and Overlake is in-network for the employee plan [info2.pdf][info4.pdf]."
 
