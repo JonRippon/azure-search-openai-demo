cbdb8ef0a8871fac6c0e76896931f79d5beb3c38
Jon Rippon <jon@rippon.me>
2024-09-25 20:58:33 +0100

    fds

--- app/backend/approaches/retrievethenread.py
+++ app/backend/approaches/retrievethenread.py
@@ -1,13 +1,17 @@
-from typing import Any, Optional
+import os
+from typing import Any, AsyncGenerator, Optional, Union
 
 from azure.search.documents.aio import SearchClient
 from azure.search.documents.models import VectorQuery
 from openai import AsyncOpenAI
-from openai.types.chat import ChatCompletionMessageParam
-from openai_messages_token_helper import build_messages, get_token_limit
 
 from approaches.approach import Approach, ThoughtStep
 from core.authentication import AuthenticationHelper
+from core.messagebuilder import MessageBuilder
+
+# Replace these with your own values, either in environment variables or directly here
+AZURE_STORAGE_ACCOUNT = os.getenv("AZURE_STORAGE_ACCOUNT")
+AZURE_STORAGE_CONTAINER = os.getenv("AZURE_STORAGE_CONTAINER")
 
 
 class RetrieveThenReadApproach(Approach):
@@ -18,22 +22,22 @@ class RetrieveThenReadApproach(Approach):
     """
 
     system_chat_template = (
-        "You are an intelligent assistant helping Contoso Inc employees with their healthcare plan questions and employee handbook questions. "
+        "You are a DORA the  Digital Operational Resilience Act, DORA falls into 5 pillars."
         + "Use 'you' to refer to the individual asking the questions even if they ask with 'I'. "
         + "Answer the following question using only the data provided in the sources below. "
+        + "For tabular information return it as an html table. Do not return markdown format. "
         + "Each source has a name followed by colon and the actual information, always include the source name for each fact you use in the response. "
         + "If you cannot answer using the sources below, say you don't know. Use below example to answer"
     )
 
     # shots/sample conversation
     question = """
-'What is the deductible for the employee plan for a visit to Overlake in Bellevue?'
+'what are the articles that need to be addressed by Small Enterprises within chapter 2 of the DORA act?'
 
 Sources:
-info1.txt: deductibles depend on whether you are in-network or out-of-network. In-network deductibles are $500 for employee and $1000 for family. Out-of-network deductibles are $1000 for employee and $2000 for family.
-info2.pdf: Overlake is in-network for the employee plan.
-info3.pdf: Overlake is the name of the area that includes a park and ride near Bellevue.
-info4.pdf: In-network institutions include Overlake, Swedish and others in the region
+The Dora Act.pdf#page=38: Articles 5 to 15 of this Regulation shall not apply to small and non-interconnected investment firms, payment
+institutions exempted pursuant to Directive (EU) 2015/2366.
+The Dora Act.pdf#page=38: identify key dependencies on ICT third-party service providers;.
 """
     answer = "In-network deductibles are $500 for employee and $1000 for family [info1.txt] and Overlake is in-network for the employee plan [info2.pdf][info4.pdf]."
 
@@ -47,7 +51,6 @@ info4.pdf: In-network institutions include Overlake, Swedish and others in the r
         chatgpt_deployment: Optional[str],  # Not needed for non-Azure OpenAI
         embedding_model: str,
         embedding_deployment: Optional[str],  # Not needed for non-Azure OpenAI or for retrieval_mode="text"
-        embedding_dimensions: int,
         sourcepage_field: str,
         content_field: str,
         query_language: str,
@@ -59,53 +62,56 @@ info4.pdf: In-network institutions include Overlake, Swedish and others in the r
         self.auth_helper = auth_helper
         self.chatgpt_model = chatgpt_model
         self.embedding_model = embedding_model
-        self.embedding_dimensions = embedding_dimensions
         self.chatgpt_deployment = chatgpt_deployment
         self.embedding_deployment = embedding_deployment
         self.sourcepage_field = sourcepage_field
         self.content_field = content_field
         self.query_language = query_language
         self.query_speller = query_speller
-        self.chatgpt_token_limit = get_token_limit(chatgpt_model)
 
     async def run(
         self,
-        messages: list[ChatCompletionMessageParam],
+        messages: list[dict],
+        stream: bool = False,  # Stream is not used in this approach
         session_state: Any = None,
         context: dict[str, Any] = {},
-    ) -> dict[str, Any]:
+    ) -> Union[dict[str, Any], AsyncGenerator[dict[str, Any], None]]:
         q = messages[-1]["content"]
-        if not isinstance(q, str):
-            raise ValueError("The most recent message content must be a string.")
         overrides = context.get("overrides", {})
-        seed = overrides.get("seed", None)
         auth_claims = context.get("auth_claims", {})
-        use_text_search = overrides.get("retrieval_mode") in ["text", "hybrid", None]
-        use_vector_search = overrides.get("retrieval_mode") in ["vectors", "hybrid", None]
-        use_semantic_ranker = True if overrides.get("semantic_ranker") else False
-        use_semantic_captions = True if overrides.get("semantic_captions") else False
+        has_text = overrides.get("retrieval_mode") in ["text", "hybrid", None]
+        has_vector = overrides.get("retrieval_mode") in ["vectors", "hybrid", None]
+        use_semantic_ranker = overrides.get("semantic_ranker") and has_text
+
+        use_semantic_captions = True if overrides.get("semantic_captions") and has_text else False
         top = overrides.get("top", 3)
         minimum_search_score = overrides.get("minimum_search_score", 0.0)
         minimum_reranker_score = overrides.get("minimum_reranker_score", 0.0)
         filter = self.build_filter(overrides, auth_claims)
-
         # If retrieval mode includes vectors, compute an embedding for the query
         vectors: list[VectorQuery] = []
-        if use_vector_search:
+        if has_vector:
             vectors.append(await self.compute_text_embedding(q))
+
+        # Only keep the text query if the retrieval mode uses text, otherwise drop it
+        query_text = q if has_text else None
 
         results = await self.search(
             top,
-            q,
+            query_text,
             filter,
             vectors,
-            use_text_search,
-            use_vector_search,
             use_semantic_ranker,
             use_semantic_captions,
             minimum_search_score,
             minimum_reranker_score,
         )
+
+        user_content = [q]
+
+        template = overrides.get("prompt_template", self.system_chat_template)
+        model = self.chatgpt_model
+        message_builder = MessageBuilder(template, model)
 
         # Process results
         sources_content = self.get_sources_content(results, use_semantic_captions, use_image_citation=False)
@@ -113,25 +119,20 @@ info4.pdf: In-network institutions include Overlake, Swedish and others in the r
         # Append user message
         content = "\n".join(sources_content)
         user_content = q + "\n" + f"Sources:\n {content}"
-
-        response_token_limit = 1024
-        updated_messages = build_messages(
-            model=self.chatgpt_model,
-            system_prompt=overrides.get("prompt_template", self.system_chat_template),
-            few_shots=[{"role": "user", "content": self.question}, {"role": "assistant", "content": self.answer}],
-            new_user_content=user_content,
-            max_tokens=self.chatgpt_token_limit - response_token_limit,
-        )
-
-        chat_completion = await self.openai_client.chat.completions.create(
-            # Azure OpenAI takes the deployment name as the model name
-            model=self.chatgpt_deployment if self.chatgpt_deployment else self.chatgpt_model,
-            messages=updated_messages,
-            temperature=overrides.get("temperature", 0.3),
-            max_tokens=response_token_limit,
-            n=1,
-            seed=seed,
-        )
+        message_builder.insert_message("user", user_content)
+        message_builder.insert_message("assistant", self.answer)
+        message_builder.insert_message("user", self.question)
+        updated_messages = message_builder.messages
+        chat_completion = (
+            await self.openai_client.chat.completions.create(
+                # Azure OpenAI takes the deployment name as the model name
+                model=self.chatgpt_deployment if self.chatgpt_deployment else self.chatgpt_model,
+                messages=updated_messages,
+                temperature=overrides.get("temperature", 0.3),
+                max_tokens=1024,
+                n=1,
+            )
+        ).model_dump()
 
         data_points = {"text": sources_content}
         extra_info = {
@@ -139,14 +140,13 @@ info4.pdf: In-network institutions include Overlake, Swedish and others in the r
             "thoughts": [
                 ThoughtStep(
                     "Search using user query",
-                    q,
+                    query_text,
                     {
                         "use_semantic_captions": use_semantic_captions,
                         "use_semantic_ranker": use_semantic_ranker,
                         "top": top,
                         "filter": filter,
-                        "use_vector_search": use_vector_search,
-                        "use_text_search": use_text_search,
+                        "has_vector": has_vector,
                     },
                 ),
                 ThoughtStep(
@@ -155,7 +155,7 @@ info4.pdf: In-network institutions include Overlake, Swedish and others in the r
                 ),
                 ThoughtStep(
                     "Prompt to generate answer",
-                    updated_messages,
+                    [str(message) for message in updated_messages],
                     (
                         {"model": self.chatgpt_model, "deployment": self.chatgpt_deployment}
                         if self.chatgpt_deployment
@@ -165,11 +165,6 @@ info4.pdf: In-network institutions include Overlake, Swedish and others in the r
             ],
         }
 
-        return {
-            "message": {
-                "content": chat_completion.choices[0].message.content,
-                "role": chat_completion.choices[0].message.role,
-            },
-            "context": extra_info,
-            "session_state": session_state,
-        }
+        chat_completion["choices"][0]["context"] = extra_info
+        chat_completion["choices"][0]["session_state"] = session_state
+        return chat_completion
